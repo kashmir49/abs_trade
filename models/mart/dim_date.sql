02671a62f1c9703a2bbe316cfc2690f9bae2108d
unknown <kashmir49@hotmail.com>
2025-05-14 12:06:34 +1000

    Add fiscal year in date table

--- models/mart/dim_date.sql
+++ models/mart/dim_date.sql
@@ -22,6 +22,18 @@ date_details as (
         cast(date_part('month', date_day) as int) as Month,
         cast(date_part('day', date_day) as int) as Day,
         cast(date_part('quarter', date_day) as int) as Quarter,
+        case
+            when date_part('month', date_day) >= 7 then date_part('year', date_day) + 1
+            else date_part('year', date_day)
+        end as Fiscal_Year,
+        case
+            when date_part('month', date_day) >= 7 then date_part('month', date_day) - 6
+            else date_part('month', date_day) + 6
+        end as Fiscal_Month,
+        case
+            when date_part('month', date_day) >= 7 then date_part('quarter', date_day) - 2
+            else date_part('quarter', date_day) + 2
+        end as Fiscal_Quarter,
         to_char(date_day, 'DY') as Day_Name,
         to_char(date_day, 'MMMM') as Month_Name,
         to_char(date_day, 'MON') as Short_Month_Name
